7015266187eed0f6f0d6b81873e5745eb527a467
gabriel.lima <gabriel.limabs02@gmail.com>
2022-06-26 01:03:58 -0300

    fix typo

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -21,7 +21,7 @@ export function Lesson(props: LessonProps) {
     })
 
     return(
-        <Link to={`/event/lesson/${props.slug}`} className='group' style={{pointerEvents: isLessonAvailable ? '' : 'none'}}>
+        <Link to={`/event/lesson/${props.slug}`} className='group'>
             <span className="text-gray-300">
                 {availableDateFormatted}
             </span>
